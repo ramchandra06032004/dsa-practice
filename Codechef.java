364395f6b005888532a0ba1d129d435740dd8661
ramchandra06032004 <rajumulik51@gmail.com>
2025-12-05 19:50:48 +0530

    feat: refactor Codechef solution to use FastIO for input handling and update input/output files

--- Codechef.java
+++ Codechef.java
@@ -1,64 +1,73 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
+public class Codechef {
+    static FastIO sc = new FastIO();
 
-class Codechef {
-
-    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        int t=sc.nextInt();
-        while(t>0){
-            t--;
-            solve();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            s();
         }
+        sc.out.flush();
     }
 
-    public static void solve() {
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        int[] prifixSum=new int[n];
-        for(int i=0 ; i<n ; i++){
-            arr[i]=sc.nextInt();
-            prifixSum[i]=((i==0?0:prifixSum[i-1])+arr[i])%3;
-        }
-        
-        int p=n-1;
-        while (p>=0) { 
-            if(prifixSum[p]==0){
-                break;
-            }
-            p--;
-        }
-        if(p==-1){
-            System.out.println("Yes");
-            return;
-        }
-        int lastZero=p;
-        
-        boolean foundOne=false;
-        boolean foundTwo=false;
+    public static void s() {
+       
+    }
+}
 
-        while(p>=0){
-            if(prifixSum[p]==1){
-                foundOne=true;
-            }
-            else if(prifixSum[p]==2){
-                foundTwo=true;
-            }
-            p--;
-        }
-        while(lastZero<n){
-            if(prifixSum[lastZero]==1 && !foundOne){
-                System.out.println("Yes");
-                return;
-            }else if(prifixSum[lastZero]==2 && !foundTwo){
-            //System.out.println(foundOne+"           "+foundTwo);
-                System.out.println("Yes");
-                return;
-            }
-            lastZero++;
-        }
-        System.out.println("No");
+class FastIO {
+    BufferedReader br;
+    StringTokenizer st;
+    PrintWriter out;
 
+    public FastIO() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+    }
 
+    String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str = "";
+        try {
+            str = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
+    void print(Object o) {
+        out.print(o);
+    }
+
+    void println(Object o) {
+        out.println(o);
     }
 }
